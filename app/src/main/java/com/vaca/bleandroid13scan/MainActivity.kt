fb589fbc3d8e7c565c0bd4e1f93abbb7cd359fd4
liguanghao <liguanghao@viatomtech.com>
2023-03-03 09:27:58 +0800

    Initial commit

---
+++ app/src/main/java/com/vaca/bleandroid13scan/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.vaca.bleandroid13scan
+
+import android.Manifest
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private fun checkPermission(p_array: List<String>): Boolean {
+        for (p in p_array) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    p
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val p_array = listOf<String>(
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_ADVERTISE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        )
+
+        val requestVoicePermission = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            start()
+        }
+        if (!checkPermission(p_array)
+        ) {
+            requestVoicePermission.launch(p_array.toTypedArray())
+        } else {
+            start()
+        }
+
+    }
+
+    private var leScanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(
+            callbackType: Int,
+            result: ScanResult,
+        ) {
+            Log.d("vaca", "result: ${result.device.address}")
+            super.onScanResult(callbackType, result)
+        }
+    }
+    private val settings: ScanSettings = ScanSettings.Builder()
+        .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
+        .build()
+
+    fun start() {
+        val bluetoothManager =
+            getSystemService(AppCompatActivity.BLUETOOTH_SERVICE) as BluetoothManager
+        val mBluetoothAdapter = bluetoothManager.adapter
+        val scanner = mBluetoothAdapter.bluetoothLeScanner
+
+        val builder = ScanFilter.Builder()
+        val filter = builder.build()
+        try {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_SCAN
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            scanner?.startScan(listOf(filter), settings, leScanCallback)
+        } catch (e: Exception) {
+
+        }
+
+    }
+}
