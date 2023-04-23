925d9b940603bb960c7877ab8f99d2d455405c05
liguanghao <liguanghao@viatomtech.com>
2023-04-23 17:20:03 +0800

    asdf515hgdf

--- app/src/main/java/com/vaca/bleandroid13scan/MainActivity.kt
+++ app/src/main/java/com/vaca/bleandroid13scan/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.vaca.bleandroid13scan
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,13 +39,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//还是android 13的蓝牙权限是画蛇添足， 还是需要精确地理位置
         val p_array = listOf<String>(
             Manifest.permission.BLUETOOTH_SCAN,
             Manifest.permission.BLUETOOTH,
             Manifest.permission.BLUETOOTH_ADMIN,
             Manifest.permission.BLUETOOTH_CONNECT,
-            Manifest.permission.BLUETOOTH_ADVERTISE,
         )
 
         val requestVoicePermission = registerForActivityResult(
@@ -55,45 +60,38 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private var leScanCallback: ScanCallback = object : ScanCallback() {
-        override fun onScanResult(
-            callbackType: Int,
-            result: ScanResult,
-        ) {
-            Log.d("vaca", "result: ${result.device.address}")
-            super.onScanResult(callbackType, result)
-        }
-    }
-    private val settings: ScanSettings = ScanSettings.Builder()
-        .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
-        .build()
 
+    @SuppressLint("MissingPermission")
     fun start() {
         val bluetoothManager =
             getSystemService(AppCompatActivity.BLUETOOTH_SERVICE) as BluetoothManager
         val mBluetoothAdapter = bluetoothManager.adapter
-        val scanner = mBluetoothAdapter.bluetoothLeScanner
-
-        val builder = ScanFilter.Builder()
-        val filter = builder.build()
-        try {
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.BLUETOOTH_SCAN
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                return
-            }
-            if(scanner==null){
-                Log.e("vacavaca", "scanner==null")
-                return
-            }
-            scanner.startScan(listOf(filter), settings, leScanCallback)
-            Log.e("vacavaca", "startScan")
-        } catch (e: Exception) {
-
-            e.printStackTrace()
+        if (mBluetoothAdapter.isDiscovering) {
+            mBluetoothAdapter.cancelDiscovery()
         }
+        mBluetoothAdapter.startDiscovery()
+        val mReceiver = SingBroadcastReceiver()
+        val ifilter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        this.registerReceiver(mReceiver, ifilter)
 
+    }
+
+    private class SingBroadcastReceiver : BroadcastReceiver() {
+        @SuppressLint("MissingPermission")
+        override fun onReceive(context: Context?, intent: Intent) {
+            val action = intent.action //may need to chain this to a recognizing function
+            if (BluetoothDevice.ACTION_FOUND == action) {
+                val device =
+                    intent.getParcelableExtra(
+                        BluetoothDevice.EXTRA_DEVICE,
+                        BluetoothDevice::class.java
+                    )
+                if (device!!.name.isNullOrEmpty()) {
+                    return
+                }
+                val derp = device.name + " - " + device.address
+                Log.e("vaca", "device: $derp")
+            }
+        }
     }
 }
